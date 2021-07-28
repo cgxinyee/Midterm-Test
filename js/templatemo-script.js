04a129cae50fc0f708b7caaca55e8b51be199094
cgxinyee <gohxinyee2703@gmail.com>
2021-07-28 09:38:11 +0800

    commit

--- js/templatemo-script.js
+++ js/templatemo-script.js
@@ -1,5 +1,5 @@
 const initBg = (autoplay = true) => {
-    const bgImgsNames = ['diagoona-bg-1.jpg', 'diagoona-bg-2.jpg', 'diagoona-bg-3.jpg'];
+    const bgImgsNames = ['Cover 1.jpg', 'Cover 2.jpg', 'Cover 4.jpg', 'Cover 7.jpg', 'Cover 8.png'];
     const bgImgs = bgImgsNames.map(img => "img/" + img);
 
     $.backstretch(bgImgs, {duration: 4000, fade: 500});
